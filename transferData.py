84258a3c42707c3884f423fc5a2a2a8ecfc64f39
LAKRUSE <Lia.Kruse@Btc-ag.com>
2024-08-14 12:27:25 +0200

    added Attributes

--- transferData.py
+++ transferData.py
@@ -1,4 +1,5 @@
 from main import getData
+import time
 
 data = getData().json()
 
@@ -8,7 +9,10 @@ if data["cod"] != "404":
    
 
     current_temperature = data['main']['temp']
-    current_weather = data['']
+    current_weather = data['weather'][0]['main']
+    current_pressure = data['main']['pressure']
+    current_cloudiness = data['clouds']['all']
+    timecode = data['dt']
+    current_time = time.ctime(timecode)
 
-
-print(current_weather)
+print(current_weather,current_pressure, current_cloudiness,current_time)
